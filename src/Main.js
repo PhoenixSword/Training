fd2143df424964dfe0783d7d69beef02327d52dd
Виталий Ляденко <czar.zarj@bk.ru>
2019-04-26 16:06:46 +0300

    commit

--- src/Main.js
+++ src/Main.js
@@ -1,34 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
 import styled from "styled-components";
-
-import NewsAccumulator from "./NewsAccumulator";
-import NewsAccumulator2 from "./NewsAccumulator2";
-import NewsAccumulator3 from "./NewsAccumulator3";
-import NewsList from "./NewsList";
-import GridDragLayer from "./GridDragLayer";
-
-const Container = styled.div`
-  width: 80%;
-  height: 100%;
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: row;
-  transition: all 3.5s ease-in-out;
-`;
-
-
-const Finish = styled.div`
-  position: absolute;
-  padding: 15% 15%;
-  width: 100%;
-  height: 100%;
-  font-size: 250px;
-  color: white;
-  transition: all 2s ease-in-out;
-  ${p => p.finish ? `opacity: 1` : `opacity: 0`};
-`;
-
+var position1 = 0;
+var position2 = 0;
+var position3 = 0;
+var correctCards = 0;
+$( init );
 
 function rand(type) {
   switch (type) {
@@ -57,17 +34,49 @@ function height(index)
     return 300 + (index%7*80) + Math.round(Math.random() *20);
 }
 
-const initialList = [
-  { id: rand(1), added: false, width: width(1), height: height(1) },
-  { id: rand(1), added: false, width: width(2), height: height(2) },
-  { id: rand(1), added: false, width: width(3), height: height(3) },
-  { id: rand(2), added: false, width: width(4), height: height(4) },
-  { id: rand(2), added: false, width: width(5), height: height(5) },
-  { id: rand(2), added: false, width: width(6), height: height(6) },
-  { id: rand(3), added: false, width: width(7), height: height(7) },
-  { id: rand(3), added: false, width: width(8), height: height(8) },
-  { id: rand(3), added: false, width: width(9), height: height(9) }
-];
+function init() {
+
+
+  // Hide the success message
+  $('#successMessage').hide();
+  $('#successMessage').css( {
+    left: '580px',
+    top: '250px',
+    width: 0,
+    height: 0
+  } );
+
+  // Reset the game
+  correctCards = 0;
+  $('#cardPile').html( '' );
+  $('#fake').html( '' );
+  position1 = 0;
+  position2 = 0;
+  position3 = 0;
+  // Create the pile of shuffled cards
+  var numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+  var terms = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10' ];
+  
+
+  for ( var i=0; i<10; i++ ) {
+    var number = rand((i+1)%3);
+    $(`<div style="left: ${height(i)}px;top:${width(i)}px" class="item"><p>${number}</p></div>`).data( 'number', number ).attr( 'id', 'card'+number ).appendTo( '#cardPile' ).draggable( {
+      stack: '#cardPile div',
+      revert: true
+    } );
+  }
+
+  // Create the card slots
+  var words = [ 'one', 'two', 'three'];
+  for ( var i=1; i<=3; i++ ) {
+    $(`<div id='cardSlots${i}'></div>`).data( 'number', i ).appendTo( `#fake` ).droppable( {
+      accept: '#cardPile div',
+      hoverClass: 'hovered',
+      drop: handleCardDrop
+    } );
+  }
+
+}
 
 function splitToDigits(number) {
   var digits = [];
@@ -78,9 +87,88 @@ function splitToDigits(number) {
   return digits;
 }
 
+function correct(element, ui, index)
+{
+    //element.droppable( 'disable' );
+    ui.draggable.addClass( 'correct' );
+    ui.draggable.draggable( 'disable' );
+    ui.draggable.draggable( 'option', 'revert', false );
+    correctCards++;
+    switch (index) {
+      case 1:
+        ui.draggable.position( { of: element, my: `left top`, at: 'left top', offset: `${position1} 0` } );
+        ui.draggable.addClass( 'rightItem' );
+        position1+=90;
+        break;
+      case 2:
+        ui.draggable.position( { of: element, my: `left top`, at: 'left top', offset: `${position2} 0` } );
+        ui.draggable.addClass( 'leftItem' );
+        position2+=90;
+        break;
+      case 3:
+        ui.draggable.position( { of: element, my: `left top`, at: 'left top', offset: `${position3} 0` } );
+        ui.draggable.addClass( 'centerItem' );
+        position3+=90;
+        break;
+      default:
+        break;
+    }
+}
+function handleCardDrop( event, ui ) {
+  var slotNumber = $(this).data( 'number' );
+  var cardNumber = ui.draggable.data( 'number' );
+
+  var masDigits = splitToDigits(cardNumber);
+  switch (slotNumber) {
+    case 1:
+      if ( masDigits[2] === 5 ) {
+        correct($(this), ui, 2);
+      }
+      else
+      {
+        ui.draggable.addClass( 'invalidItem' );
+        setTimeout(function(){
+          ui.draggable.removeClass( 'invalidItem' );
+        }, 500);
+      }
+      break;
+    case 2:
+      if ( masDigits[1] === 2 ) {
+        correct($(this), ui, 1);
+      }
+      else
+      {
+        ui.draggable.addClass( 'invalidItem' );
+        setTimeout(function(){
+          ui.draggable.removeClass( 'invalidItem' );
+        }, 500);
+      }
+      break;
+    case 3:
+      if ( ( masDigits[1] !== 2 ) && ( masDigits[2] !== 5 )  ) {
+        correct($(this), ui, 3);
+      }
+      else
+      {
+        ui.draggable.addClass( 'invalidItem' );
+        setTimeout(function(){
+          ui.draggable.removeClass( 'invalidItem' );
+        }, 500);
+      }
+      break;
+    default:
+      break;
+  }
+  if ( correctCards == 1 ) {
+    $('#cardSlots1').addClass('animated');
+    $('#cardSlots2').addClass('animated');
+    $('#cardSlots3').addClass('animated');
+  }
+
+}
+
 class Main extends React.Component {
   state = {
-    list: initialList,
     finish: false,
     addedList: [],
     addedList2: [],
@@ -88,100 +176,24 @@ class Main extends React.Component {
     error: ""
   };
 
-  handleDropNews = id => {
-    if (splitToDigits(id)[2] === 5){
-      this.setState(state => ({
-        addedList: state.addedList.concat([
-          state.list.find(news => news.id === id)
-        ]),
-        list: state.list.filter(news => news.id !== id)
-      }));
-      this.checkFinish();
-    }
-    else
-    {
-      this.setState(state => ({
-        error: id
-      }));
-    }
-
-  };
-  handleDropNews2 = id => {
-    if (splitToDigits(id)[1] === 2) {
-      this.setState(state => ({
-        addedList2: state.addedList2.concat([
-          state.list.find(news => news.id === id)
-        ]),
-        list: state.list.filter(news => news.id !== id)
-      }));
-      this.checkFinish();
-    }
-    else
-    {
-      this.setState(state => ({
-        error: id
-      }));
-    }
-  };
-
-  handleDropNews3 = id => {
-    if ((splitToDigits(id)[1] !== 2) && (splitToDigits(id)[2] !== 5)) {
-      this.setState(state => ({
-        addedList3: state.addedList3.concat([
-          state.list.find(news => news.id === id)
-        ]),
-        list: state.list.filter(news => news.id !== id)
-      }));
-      this.checkFinish();
-    }
-    else
-    {
-      this.setState(state => ({
-        error: id
-      }));
-    }
-  };
-
-
-  checkFinish()
-  {
-    this.state.list.length === 0 ? 
-    (
-      this.setState(state => ({
-        finish: true
-      }))
-    ) : "";
-  }
   render() {
-    const { error, list, addedList, addedList2, addedList3, finish } = this.state;
-    
     return (
-      
-      <Container>
+      <React.Fragment>
 
-      <GridDragLayer />
+    <div id="content">
+      <div id="fake"></div>
+      <div id="cardPile"> </div>
 
-      <Finish finish={finish}>Finish</Finish>
-        <NewsAccumulator2
-          list={addedList2}
-          setNewsToAccumulator={this.handleDropNews2}
-          finish={finish}
-        />
 
-        <NewsList list={list} error={error}/>
 
-        <NewsAccumulator
-          list={addedList}
-          setNewsToAccumulator={this.handleDropNews}
-          finish={finish}
-        />
-        <NewsAccumulator3
-          list={addedList3}
-          setNewsToAccumulator={this.handleDropNews3}
-          finish={finish}
-        />
-      </Container>
-     
+      <div id="successMessage">
+        <h2>Congratulations!</h2>
+        <button onClick={init}>Play Again</button>
+      </div>
+
+    </div>
+
+    </React.Fragment>
     );
   }
 }
