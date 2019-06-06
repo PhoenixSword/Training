5ebadd40207327e3f71088e098f7c76a88a14619
Виталий Ляденко <czar.zarj@bk.ru>
2019-06-06 16:08:48 +0300

    Some Fixes

---
+++ Training/ClientApp/src/components/Game2Settings.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+import {userService} from "./services/UserService";
+import {teacherService} from "./services/TeacherService";
+import { MDBBtn, MDBInput } from "mdbreact";
+import Notification from "./Notification";
+import $ from "jquery";
+
+const Settings = () => 
+{
+  return {
+    cardsCount: 0
+  }
+}
+
+export class Game2Settings extends Component {
+  static displayName = Game2Settings.name;
+  constructor (props) {
+    super(props);
+      var settings = this.props.event.settings !== null ? JSON.parse(this.props.event.settings) : [];
+    this.state = 
+    {
+      eventId: this.props.event.id,
+      settings : settings
+    }
+    this.service = userService;
+    this.teacherService = teacherService;
+    this.add = this.add.bind(this);
+    this.save = this.save.bind(this);
+    this.remove = this.remove.bind(this);
+    this.add = this.add.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  save(){
+    this.teacherService.saveSettings(this.state.eventId, this.state.settings).then(
+      (response)=> {
+        Notification("Сохранено");
+        $(".settingsBlock").hide(); 
+        $('#overlay2').hide(); 
+      });
+  }
+
+  add(){
+    this.setState(prevState => ({
+        settings: [...prevState.settings, Settings()]}), () => {})
+  }
+
+  remove(id, index){
+    this.state.settings.splice(index, 1)
+    this.setState(
+      prevState => ({
+            settings: this.state.settings
+          }));
+  }
+
+  onChange(e){
+    var val = e.target.value;
+    var index = e.target.parentNode.parentNode.parentNode.parentNode.id;
+    var stateCopy = Object.assign({}, this.state);
+    stateCopy.settings = stateCopy.settings.slice();
+    stateCopy.settings[index] = Object.assign({}, stateCopy.settings[index]);
+    stateCopy.settings[index].cardsCount = +val;
+    this.setState(stateCopy);
+  }
+  render () {
+    return ( 
+      <div>
+        <table className="table">
+              <thead className="blue-gradient white-text">
+                <tr>
+                  <th className="">Уровень</th>
+                  <th className="">Количество кристаллов</th>
+                  <th className="">Удалить</th>
+                </tr>
+              </thead>
+              <tbody>
+              {
+              this.state.settings.map((item, index) =>
+                <tr key={index} id={index}>
+                  <td>
+                    <div className="md-form text-center">
+                    <MDBInput readOnly type="number" name="levelNumber" value={(index+1).toString()} disabled/>
+                    </div>
+                  </td>
+                  <td>
+                    <div className="md-form">
+                      <MDBInput type="number" name="cardsCount" value={item.cardsCount.toString()} onChange={this.onChange} min="1" max="11"/>
+                    </div>
+                  </td>
+                  <td><MDBBtn style={{padding: "5px 20px"}} onClick={() => this.remove(item.id, index)} color="danger">Удалить</MDBBtn></td>
+                </tr>
+                )
+              }
+              </tbody>
+            </table>
+              <MDBBtn color="primary" onClick={this.add}>Добавить уровень</MDBBtn>
+              <MDBBtn color="success" onClick={this.save}>Сохранить</MDBBtn>
+        </div>
+    );
+  }
+}
---
+++ Training/Models/ViewModel/SettingsModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Training.Models.ViewModel
+{
+    public class SettingsModel
+    {
+        public string Id { get; set; }
+        public object Settings { get; set; }
+    }
+}
